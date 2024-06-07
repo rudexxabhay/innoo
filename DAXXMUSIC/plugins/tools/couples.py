720f425b2c259f1026be4d7a78d7dc3c2d5ffdee
Mr Broken <74880754+mrxbroken011@users.noreply.github.com>
2024-06-07 10:06:43 +0530

    Update couples.py

--- DAXXMUSIC/plugins/tools/couples.py
+++ DAXXMUSIC/plugins/tools/couples.py
@@ -14,8 +14,8 @@ from DAXXMUSIC.mongo.couples_db import _get_image, get_couple
 POLICE = [
     [
         InlineKeyboardButton(
-            text="𓊈𒆜彡[𝐃𝚊𝚡𝚡 𝐂𝙲 𝐂𝙻𝚄𝙱 ]彡𒆜𓊉",
-            url=f"https://t.me/ALLTYPECC",
+            text="𝔹𝕣𝕠𝕜𝕖𝕟 𝕏 ℕ𝕖𝕥𝕨𝕠𝕣𝕜 ☠️",
+            url=f"https://t.me/BROKENXNETWORK",
         ),
     ],
 ]
