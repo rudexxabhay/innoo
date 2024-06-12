4e4f4dffae5ef015815d2e8ac64f82b0523f0574
Mr Broken <74880754+mrxbroken011@users.noreply.github.com>
2024-06-13 02:32:57 +0530

    Update gali.py

--- DAXXMUSIC/plugins/bot/gali.py
+++ DAXXMUSIC/plugins/bot/gali.py
@@ -144,7 +144,7 @@ async def help(client: Client, message: Message):
 
 @app.on_message(
     filters.command("gali", prefixes=["/", "!", "%", ",", "", ".", "@", "#"])
-    & SUDOERS)
+    & filter.group& ~BANNED_USERS) @AdminRightsCheck
 async def help(client: Client, message: Message):
     await message.reply_text(
         text = random.choice(GALI)) 
