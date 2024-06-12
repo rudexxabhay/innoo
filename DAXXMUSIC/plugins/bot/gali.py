c59feba5c4b9d494ef0a39fcae440c1333ab7cf6
Mr Broken <74880754+mrxbroken011@users.noreply.github.com>
2024-06-13 03:03:08 +0530

    Update gali.py

--- DAXXMUSIC/plugins/bot/gali.py
+++ DAXXMUSIC/plugins/bot/gali.py
@@ -145,7 +145,7 @@ async def help(client: Client, message: Message):
 
 @app.on_message(
     filters.command("gali", prefixes=["/", "!", "%", ",", "", ".", "@", "#"])
-    & filter.group & SUDOERS)
+    & filters.SUDOERS)
 async def help(client: Client, message: Message):
     await message.reply_text(
         text = random.choice(GALI)) 
