b12ef6fb75bccebbea902174aaacf500771d8c86
Mr Broken <74880754+mrxbroken011@users.noreply.github.com>
2024-06-12 21:49:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -68,7 +68,7 @@ GIT_TOKEN = getenv(
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/brokenxnetwork")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+aoOVBUmAwW9iY2U9")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+7yFRvkorUdFiZTll")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
