01b7e410ccac6a49121628b4d04b3a65876b46ef
Mr Broken <74880754+mrxbroken011@users.noreply.github.com>
2024-07-02 21:43:19 +0530

    Update misc.py

--- DAXXMUSIC/misc.py
+++ DAXXMUSIC/misc.py
@@ -1,7 +1,7 @@
 import socket
 import time
 
-import heroku3
+#import heroku3
 from pyrogram import filters
 
 import config
@@ -28,9 +28,9 @@ XCB = [
     "git",
     "heroku",
     "push",
-    str(config.HEROKU_API_KEY),
+    #str(config.HEROKU_API_KEY),
     "https",
-    str(config.HEROKU_APP_NAME),
+   # str(config.HEROKU_APP_NAME),
     "HEAD",
     "master",
 ]
@@ -61,15 +61,15 @@ async def sudo():
     LOGGER(__name__).info(f"𝗦𝗨𝗗𝗢 𝗨𝗦𝗘𝗥 𝗗𝗢𝗡𝗘✨🎋.")
 
 
-def heroku():
-    global HAPP
-    if is_heroku:
-        if config.HEROKU_API_KEY and config.HEROKU_APP_NAME:
-            try:
-                Heroku = heroku3.from_key(config.HEROKU_API_KEY)
-                HAPP = Heroku.app(config.HEROKU_APP_NAME)
-                LOGGER(__name__).info(f"🍟𝗛𝗘𝗥𝗢𝗞𝗨 𝗔𝗣𝗣 𝗡𝗔𝗠𝗘 𝗟𝗢𝗔𝗗......💦")
-            except BaseException:
-                LOGGER(__name__).warning(
-                    f"✨𝐘𝐨𝐮 𝐇𝐚𝐯𝐞 𝐍𝐨𝐭 𝐅𝐢𝐥𝐥𝐞𝐝 𝐇𝐞𝐫𝐨𝐤𝐮 𝐀𝐩𝐢 𝐊𝐞𝐲 𝐀𝐧𝐝 𝐇𝐞𝐫𝐨𝐤𝐮 𝐀𝐩𝐩 𝐍𝐚𝐦𝐞 🕊️𝐂𝐨𝐫𝐫𝐞𝐜𝐭...."
-)
+#def heroku():
+   # global HAPP
+ #   if is_heroku:
+       # if config.HEROKU_API_KEY and config.HEROKU_APP_NAME:
+         #   try:
+           #     Heroku = heroku3.from_key(config.HEROKU_API_KEY)
+           #     HAPP = Heroku.app(config.HEROKU_APP_NAME)
+         #       LOGGER(__name__).info(f"🍟𝗛𝗘𝗥𝗢𝗞𝗨 𝗔𝗣𝗣 𝗡𝗔𝗠𝗘 𝗟𝗢𝗔𝗗......💦")
+     #       except BaseException:
+        #        LOGGER(__name__).warning(
+           #         f"✨𝐘𝐨𝐮 𝐇𝐚𝐯𝐞 𝐍𝐨𝐭 𝐅𝐢𝐥𝐥𝐞𝐝 𝐇𝐞𝐫𝐨𝐤𝐮 𝐀𝐩𝐢 𝐊𝐞𝐲 𝐀𝐧𝐝 𝐇𝐞𝐫𝐨𝐤𝐮 𝐀𝐩𝐩 𝐍𝐚𝐦𝐞 🕊️𝐂𝐨𝐫𝐫𝐞𝐜𝐭...."
+#)
