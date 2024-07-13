f198d64583f3ea66ec3f34c55c4f83bc8229926a
Mr Broken <74880754+mrxbroken011@users.noreply.github.com>
2024-07-13 19:38:12 +0530

    Update mustjoin.py

--- DAXXMUSIC/plugins/bot/mustjoin.py
+++ DAXXMUSIC/plugins/bot/mustjoin.py
@@ -1,36 +1,37 @@
+
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
 from DAXXMUSIC import app
 
-MUST_JOIN = "KGF_ROCY"
+#--------------------------
 
+MUST_JOIN = "brokenxnetwork"
+#------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
     if not MUST_JOIN:
         return
-    
     try:
-        await app.get_chat_member(MUST_JOIN, msg.from_user.id)
-    except UserNotParticipant:
-        link = f"https://t.me/{MUST_JOIN}" if MUST_JOIN.isalpha() else (await app.get_chat(MUST_JOIN)).invite_link
         try:
-            await msg.reply_photo(
-                photo="https://telegra.ph/file/dfde87c32f8951c6b7628.jpg", 
-                caption=(
-                    "๏ ᴀᴄᴄᴏʀᴅɪɴɢ ᴛᴏ ᴍʏ ᴅᴀᴛᴀʙᴀsᴇ ʏᴏᴜ'ᴠᴇ ɴᴏᴛ ᴊᴏɪɴᴇᴅ "
-                    "[๏sᴜᴘᴘᴏʀᴛ๏](https://t.me/Dasi_girl_boys_video_chat_group) ʏᴇᴛ, ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴜsᴇ ᴍᴇ "
-                    "ᴛʜᴇɴ ᴊᴏɪɴ [๏sᴜᴘᴘᴏʀᴛ๏](https://t.me/Dasi_girl_boys_video_chat_group) ᴀɴᴅ sᴛᴀʀᴛ ᴍᴇ ᴀɢᴀɪɴ !\n\n"
-                    "𝐏ᴏᴡᴇʀᴇᴅ  𝐁ʏ : `[—͟͞™ɪɴɴᴏᴄ͢͢͢ᴇɴᴛ आत्मा </𝟑 ｡˚](t.me/KGF_ROKY)💥"
-                ),
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [InlineKeyboardButton("๏Jᴏɪɴ๏", url=link)]
-                    ]
+            await app.get_chat_member(MUST_JOIN, msg.from_user.id)
+        except UserNotParticipant:
+            if MUST_JOIN.isalpha():
+                link = "https://t.me/" + MUST_JOIN
+            else:
+                chat_info = await app.get_chat(MUST_JOIN)
+                link = chat_info.invite_link
+            try:
+                await msg.reply_photo(
+                    photo="https://telegra.ph/file/91c6683a0074d9dce03c1.jpg", caption=f"๏ ᴀᴄᴄᴏʀᴅɪɴɢ ᴛᴏ ᴍʏ ᴅᴀᴛᴀʙᴀsᴇ ʏᴏᴜ'ᴠᴇ ɴᴏᴛ ᴊᴏɪɴᴇᴅ [๏ᴄʀᴇᴀᴛᴏʀ๏](http://t.me/aboutbrokenx) ʏᴇᴛ, ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴜsᴇ ᴍᴇ ᴛʜᴇɴ ᴊᴏɪɴ [๏ᴄʀᴇᴀᴛᴏʀ๏](http://t.me/aboutbrokenx) ᴀɴᴅ sᴛᴀʀᴛ ᴍᴇ ᴀɢᴀɪɴ ! ",
+                    reply_markup=InlineKeyboardMarkup(
+                        [
+                            [InlineKeyboardButton("๏ᴄʀᴇᴀᴛᴏʀ๏", url=link)]
+                        ]
+                    )
                 )
-            )
-            await msg.stop_propagation()
-        except ChatWriteForbidden:
-            pass
+                await msg.stop_propagation()
+            except ChatWriteForbidden:
+                pass
     except ChatAdminRequired:
-        print(f"๏ᴘʀᴏᴍᴏᴛᴇ ᴍᴇ ᴀs ᴀɴ ᴀᴅᴍɪɴ ɪɴ ᴛʜᴇ ᴍᴜsᴛ_Jᴏɪɴ ᴄʜᴀᴛ ๏: {MUST_JOIN} !`💥")
+        print(f"๏ᴘʀᴏᴍᴏᴛᴇ ᴍᴇ ᴀs ᴀɴ ᴀᴅᴍɪɴ ɪɴ ᴛʜᴇ ᴍᴜsᴛ_Jᴏɪɴ ᴄʜᴀᴛ ๏: {MUST_JOIN} !")
